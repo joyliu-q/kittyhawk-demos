b44a568362d1ae944dcb6560b567529e43b762cb
Joy Liu <joyliu.q@gmail.com>
2022-03-09 13:49:07 -0500

    :bug: Fix Clubs Secret Bug

--- src/config/clubs.ts
+++ src/config/clubs.ts
@@ -5,96 +5,93 @@ import { ReactApplication, DjangoApplication, RedisApplication, CronJob } from '
 const cronTime = require('cron-time-generator');
 
 export class ClubsChart extends Chart {
-  constructor(scope: Construct, id: string, props: ChartProps = { }) {
+  constructor(scope: Construct, id: string, props: ChartProps = {}) {
     super(scope, id, props);
 
+    /** Shared */
     const backendImage = 'pennlabs/penn-clubs-backend';
+    const frontendImage = 'pennlabs/penn-clubs-frontend';
+
+    /** Clubs */
     const clubsSecret = 'penn-clubs';
-    const fyhSecret = 'first-year-hub';
     const clubsDomain = 'pennclubs.com';
-    const fyhDomain = 'hub.provost.upenn.edu';
-
-    const clubsDjangoCommon = {
-      deployment: {
-        image: backendImage,
-        env: [
-          { name: 'REDIS_HOST', value: 'penn-clubs-redis' },
-        ],
-      },
-      secret: clubsSecret,
-      djangoSettingsModule: 'pennclubs.settings.production',
-    };
-
-    const fyhDjangoCommon = {
-      deployment: {
-        image: backendImage,
-        env: [
-          { name: 'REDIS_HOST', value: 'penn-clubs-hub-redis' },
-          { name: 'NEXT_PUBLIC_SITE_NAME', value: 'fyh' },
-        ],
-      },
-      secret: fyhSecret,
-      djangoSettingsModule: 'pennclubs.settings.production',
-    };
-
 
     new RedisApplication(this, 'redis', {});
 
     new DjangoApplication(this, 'django-asgi', {
-      ...clubsDjangoCommon,
       deployment: {
-        image: clubsDjangoCommon.deployment.image,
+        image: backendImage,
         cmd: ['/usr/local/bin/asgi-run'],
         replicas: 2,
+        secret: clubsSecret,
+        env: [
+          { name: 'REDIS_HOST', value: 'penn-clubs-redis' },
+        ],
       },
-      domains: [{ host: clubsDomain, paths: ['/api/ws']}],
+      djangoSettingsModule: 'pennclubs.settings.production',
+      domains: [{ host: clubsDomain, paths: ['/api/ws'] }],
     });
 
     new DjangoApplication(this, 'django-wsgi', {
-      ...clubsDjangoCommon,
       deployment: {
-        image: clubsDjangoCommon.deployment.image,
+        image: backendImage,
         replicas: 5,
+        secret: clubsSecret,
+        env: [
+          { name: 'REDIS_HOST', value: 'penn-clubs-redis' },
+        ],
       },
-      domains: [{ host: clubsDomain, paths: ['/api']}],
+      djangoSettingsModule: 'pennclubs.settings.production',
+      domains: [{ host: clubsDomain, paths: ['/api'] }],
     });
 
     new ReactApplication(this, 'react', {
       deployment: {
-        image: 'pennlabs/penn-clubs-frontend',
+        image: frontendImage,
         replicas: 2,
       },
       domain: { host: clubsDomain, paths: ['/'] },
       portEnv: '80',
     });
-
+    
     /** FYH */
+    const fyhSecret = 'first-year-hub';
+    const fyhDomain = 'hub.provost.upenn.edu';
 
     new RedisApplication(this, 'hub-redis', {});
 
     new DjangoApplication(this, 'hub-django-asgi', {
-      ...fyhDjangoCommon,
       deployment: {
-        image: fyhDjangoCommon.deployment.image,
+        image: backendImage,
         cmd: ['/usr/local/bin/asgi-run'],
         replicas: 2,
+        secret: fyhSecret,
+        env: [
+          { name: 'REDIS_HOST', value: 'penn-clubs-hub-redis' },
+          { name: 'NEXT_PUBLIC_SITE_NAME', value: 'fyh' },
+        ],
       },
+      djangoSettingsModule: 'pennclubs.settings.production',
       domains: [{ host: fyhDomain, paths: ['/api/ws'] }],
     });
 
     new DjangoApplication(this, 'hub-django-wsgi', {
-      ...fyhDjangoCommon,
       deployment: {
         image: backendImage,
         replicas: 3,
+        secret: fyhSecret,
+        env: [
+          { name: 'REDIS_HOST', value: 'penn-clubs-hub-redis' },
+          { name: 'NEXT_PUBLIC_SITE_NAME', value: 'fyh' },
+        ],
       },
+      djangoSettingsModule: 'pennclubs.settings.production',
       domains: [{ host: fyhDomain, paths: ['/api'] }],
     });
 
-
     new ReactApplication(this, 'hub-react', {
       deployment: {
-        image: 'pennlabs/penn-clubs-frontend',
+        image: frontendImage,
         replicas: 2,
         env: [
           { name: 'NEXT_PUBLIC_SITE_NAME', value: 'fyh' },
@@ -105,7 +102,6 @@ export class ClubsChart extends Chart {
     });
 
     /** Cronjobs **/
-
     new CronJob(this, 'rank-clubs', {
       schedule: cronTime.everyDayAt(8),
       image: backendImage,
